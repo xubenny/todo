cb00b3795721bc80794a331e2673ffacf47b41b0
xubenny <xubinglin@BINGLINs-iMac.home>
2016-03-16 15:20:55 -0700

    authenticate by passport

--- public/javascripts/userController.js
+++ public/javascripts/userController.js
@@ -2,7 +2,11 @@ todoApp.controller('userController', ['$scope', '$http', function($scope, $http)
     
     $scope.logined = false;
     $scope.email = "";
-    
+
+    // Every time a modal is shown, if it has an autofocus element, focus on it.
+    $('.modal').on('shown.bs.modal', function() {
+        $(this).find('[autofocus]').focus();
+    });
     
     $scope.onSignin = function(email, pw) {
         
@@ -21,17 +25,20 @@ todoApp.controller('userController', ['$scope', '$http', function($scope, $http)
                 $("#signin").modal('hide');
                 
             },
-            // error call back
+            // fail call back
             function (res) {
                 
                 console.log("post/users/login fail", res.data, res.status);
                 $scope.logined = false;
                 
-                var msg = res.data.error;
-
+                var errMsg = res.data;
+                if(res.status === 401) {
+                    errMsg = 'Email or password does not match!';
+                }
+                
                 // tell user name or password not correct
                 if($("#loginFail").length === 0) {
-                    $("#signinForm").prepend("<div class='alert alert-danger alert-dismissible' id='loginFail' role='alert'> <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>" + msg + "</div>");
+                    $("#signinForm").prepend("<div class='alert alert-danger alert-dismissible' id='loginFail' role='alert'> <button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>" + errMsg + "</div>");
                 }
                 $('#signin').find('[autofocus]').focus();
             }
--- routes/users.js
+++ routes/users.js
@@ -2,34 +2,65 @@ var express = require('express');
 var router = express.Router();
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/todo');
+var passport = require('passport');
+var passportLocal = require('passport-local');
+var expressSession = require('express-session');
 
-users = mongoose.model('users', {
+//-------------------- mongoDB -------------------
+mongoose.connect('mongodb://localhost/todo');
+var users = mongoose.model('users', {
     email: String,
     password: String
 });
 
+//-------------------- passport ------------------
+router.use(expressSession({ 
+	secret: process.env.SESSION_SECRET || 'looking for job',
+	resave: false,
+	saveUninitialized: false
+}));
+router.use(passport.initialize());
+router.use(passport.session());
 
-router.post('/login', function(req, res) {
+passport.use(new passportLocal.Strategy({
+        usernameField: 'email',
+        passwordField: 'password'
+    },
+    function(email, password, done) {
 
-    console.log('post/users/login', req.body);
+        users.findOne({email: email, password: password}, {_id:0, email:1}, function(err, user) {
+            // if success, db will give an object {_id:..., email:..., password:...}
+            // if fail, db will give a 'null'
+            console.log("passportLocal.Strategy", user);
+            done(null, user);
+        });
+    })
+);
+
+// the 'user' param is from passportLocal.Strategy
+// serialize mean from user to an unique ID
+passport.serializeUser(function(user, done) {
+    console.log("passport.serializeUser", user);
+    done(null, user.email);
+});
+
+// deserialize mean from unique ID to user
+passport.deserializeUser(function(email, done) {
+    users.findOne({email: email}, {_id:0, email:1}, function(err, user) {
+        console.log("passport.deserializeUser", user);
+        done(null, user);
+    })
+});
+
+//-------------------- router --------------------
+
+router.post('/login', passport.authenticate('local'), function(req, res) {
+
+    // req.isAuthenticated() is not needed, passport will return '401' error (Unauthorized) if fail
+    console.log('post/users/login success', req.body);
     
-    users.findOne({email: req.body.email, password: req.body.password}, 
-        function(err, user) {
-        
-            // err mean system access error
-            if(err) return console.error(err);
-        
-            if(user) {
-                console.log('login success');
-                res.json({email: req.body.email});
-            }
-            else {
-                console.log('login fail');
-                res.status(500).send({ error: "Email or password does not match!" });
-            }
-        }
-    );
+    res.json(req.user);
+    console.log("success", req.user);
 })
 
 module.exports = router;
